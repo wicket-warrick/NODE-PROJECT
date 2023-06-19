2a617b1bcb0bc8f4397f606dbcfcbf5be7f7ea71
Hugo Suarez <hugolgsuarez@gmail.com>
2023-06-19 23:57:38 +0200

    server

--- server.js
+++ server.js
@@ -159,9 +159,9 @@ app.use((error, req, res, next) => {
 });
 
 // const { API_PORT, API_HOST } = process.env;
-const API_PORT=process.env.API_PORT
-console.log(API_PORT)
-app.listen(API_PORT || 3000, () => {
+const API_PORT=process.env.API_PORT || 3000
+// console.log(API_PORT)
+app.listen(API_PORT, () => {
   console.log(
     chalk.black.bgWhite.bold(`API funcionando en puerto ${API_PORT}`)
   );
